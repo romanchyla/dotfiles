5886fdb2e61e6b98e37851b9f53977be326ebc08
Roman Chyla <roman.chyla@gmail.com>
2015-11-29 23:14:29 -0500

    next

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,6 +5,6 @@ CWD=`pwd`
 git config --local filter.openssl.smudge "$CWD/gitencrypt/smudge_filter_openssl"
 git config --local filter.openssl.clean "$CWD/gitencrypt/clean_filter_openssl"
 git config --local diff.openssl.textconv "$CWD/gitencrypt/diff_filter_openssl"
-
+git config --local merge.renormalize true
 
 
