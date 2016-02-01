6ca2c8f8e67ae5f048067382717671d74f58ed0f
Roman Chyla <roman.chyla@gmail.com>
2016-02-01 11:27:53 -0500

    Saving the config for argo machine

--- provision.secret/dconf/set-config.sh
+++ provision.secret/dconf/set-config.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-# necessary to get the values
-unset XDG_CONFIG_HOME
+source dconf.setup
 
 gsettings set org.gnome.desktop.wm.keybindings switch-input-source "['<Shift>Shift_R']"
 gsettings set org.gnome.desktop.wm.keybindings switch-input-source-backward "['']"
+gsettings set org.gnome.desktop.input-sources sources "[('xkb', 'us'), ('xkb', 'cz+qwerty')]"
+gsettings set org.freedesktop.ibus.general engines-order "['xkb:us::eng', 'xkb:cz:qwerty:cze']"
