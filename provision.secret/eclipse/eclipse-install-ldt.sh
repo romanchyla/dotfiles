a69e051b3bad4f7bebccc5f8181ee300e714add0
Roman Chyla <roman.chyla@gmail.com>
2016-02-02 23:11:44 -0500

    Adding LDT

---
+++ provision.secret/eclipse/eclipse-install-ldt.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# this should be run insie the eclipse folder, it will install pydev, to remove it pass 'remove'
+
+repository=`cat ~/dotfiles/provision.secret/eclipse/eclipse-repos`
+
+pushd $1
+
+if [ "$2" == "remove" ]; then
+  action='uninstallIUs'
+else
+  action='installIUs'
+fi
+
+./eclipse -nosplash -application org.eclipse.equinox.p2.director -repository  "$repository" -${action} "org.eclipse.dltk.sh.feature.group"
+
+popd
