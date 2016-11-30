e62ab1a9a4369b4c5414052f21d1096e833f855c
Roman Chyla <roman.chyla@gmail.com>
2016-11-30 06:16:43 -0500

    From laptop

--- bin/eclipse.sh
+++ bin/eclipse.sh
@@ -4,5 +4,10 @@ export UBUNTU_MENUPROXY=0
 export SWT_GTK3=0 
 export IBUS_ENABLE_SYNC_MODE=1
 
-cd /dvt/eclipse-neon/eclipse
+if [ "$1" = "cpp" ]; then
+  cd /dvt/eclipse-neon-cpp/eclipse
+else
+  cd /dvt/eclipse-neon/eclipse
+fi
+
 ./eclipse -showlocation
